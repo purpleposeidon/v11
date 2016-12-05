015ab2c1cf49bf4a39c4455a9f39fc01a88bdf60
neptunepink <purpleposeidon@gmail.com>
2016-12-04 23:55:08 -0800

    JoinCore improvements
    * Shorter name since there's only one, and if we add other variants
    they're probably less useful & thus should have longer names
    * Take a user-supplied cmp instead of an Ord type parameter.
    * Return a custom enum instead of Option to control break/continue.

--- src/joincore.rs
+++ src/joincore.rs
@@ -11,11 +11,16 @@ use std::iter::Peekable;
  * ```
  * # extern crate v11;
  * # fn main() {
+ * # use v11::joincore::*;
  * let left = vec![2, 3, 5, 7, 11];
- * let mut right = v11::joincore::InnerJoinCore::new(vec![1, 3, 4, 5, 7, 9].into_iter());
+ * let mut right = JoinCore::new(vec![1, 3, 4, 5, 7, 9].into_iter());
  * for prime in left.iter() {
- *    if let Some(odd) = right.join(*prime) {
- *        println!("What an odd prime number: {}", odd);
+ *    match right.join(prime, i32::cmp) {
+ *        Join::Next => continue,
+ *        Join::Stop => break,
+ *        Join::Match(odd) => {
+ *            println!("What an odd prime number: {}", odd);
+ *        },
  *    }
  * };
  * # }
@@ -28,29 +33,26 @@ use std::iter::Peekable;
  * dealing with two streams that are both actually `Iterator`s.
  *
  * */
-pub struct InnerJoinCore<I, IT: Iterator<Item=I>> {
+pub struct JoinCore<I, IT: Iterator<Item=I>> {
     right: Peekable<IT>,
 }
-impl<I, IT: Iterator<Item=I>> InnerJoinCore<I, IT> where I: Ord {
+impl<I, IT: Iterator<Item=I>> JoinCore<I, IT> where I: Ord {
     pub fn new(iter: IT) -> Self {
-        InnerJoinCore {
+        JoinCore {
             right: iter.peekable(),
         }
     }
 
-    pub fn join(&mut self, left_item: I) -> Option<I> {
-        loop {
-            let cmp = if let Some(right_item) = self.right.peek() {
-                left_item.cmp(right_item)
-            } else {
-                // It would be nice to signal an early break. Hmm!
-                return None;
-            };
-            return match cmp {
+    pub fn join<Compare>(&mut self, left_item: &I, cmp: Compare) -> Join<I>
+        where Compare: Fn(&I, &I) -> Ordering
+    {
+        while self.right.peek().is_some() {
+            let right_item = self.right.next().unwrap();
+            return match cmp(left_item, &right_item) {
                 // left_item needs to advance
-                Ordering::Less => None,
+                Ordering::Less => Join::Next,
                 // a good join
-                Ordering::Equal => Some(left_item),
+                Ordering::Equal => Join::Match(right_item),
                 // the right side needs to advance
                 Ordering::Greater => {
                     self.right.next();
@@ -58,6 +60,20 @@ impl<I, IT: Iterator<Item=I>> InnerJoinCore<I, IT> where I: Ord {
                 },
             };
         }
+        Join::Stop
     }
 }
 
+/// Return value for `JoinCore.join`.
+pub enum Join<T> {
+    /// The outer/left iterator needs to be advanced.
+    Next,
+    /// The inner/right iterator has no more elements.
+    Stop,
+    /// A matching result.
+    Match(T),
+}
+
+
+// FIXME: More tests.
+// FIXME: Extract separate crate.
