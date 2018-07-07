7ea5da16b72edc99b96993e2e38f0970280df583
neptunepink <purpleposeidon@gmail.com>
2018-07-06 20:32:24 -0700

    Revert that.
    This reverts commit 7d81515bb0173c56c0c47725dff351d2d75b6df5.

--- macros/output.rs
+++ macros/output.rs
@@ -1330,11 +1330,11 @@ pub fn write_out<W: Write>(table: Table, mut out: W) -> ::std::io::Result<()> {
     out! { ["`context!` duck-type implementation"] {
         use self::v11::context::Lockable;
 
-        impl<'u> Lockable<'u> for Write<'u> {
+        unsafe impl<'u> Lockable<'u> for Write<'u> {
             const TYPE_NAME: &'static str = concat!("mut v11/table/", #TABLE_PATH_STR);
             fn lock(universe: &'u Universe) -> Self { write(universe) }
         }
-        impl<'u> Lockable<'u> for Read<'u> {
+        unsafe impl<'u> Lockable<'u> for Read<'u> {
             const TYPE_NAME: &'static str = concat!("ref v11/table/", #TABLE_PATH_STR);
             fn lock(universe: &'u Universe) -> Self { read(universe) }
         }
--- src/context.rs
+++ src/context.rs
@@ -4,38 +4,38 @@
 //!
 //! 1. A higher function may have a write lock, and some lower function also needs a write locking,
 //!    resulting in a dead-lock.
-//! 2. Passing many locks around is unwieldy, but must be done within tight `for` loops.
+//! 2. Passing many locks around is unwieldy, but must be done in tight `for` loops.
 //!
 //! You could create context structs manually, but this is labor-intensive, especially when you
 //! start needing to combine them.
 //!
 //! This module introduces [`context!`] to help with this.
-use std::any::Any;
+use std::os::raw::c_void;
 use Universe;
 
 
-/// A struct that can be moved piece-wise.
+#[doc(hidden)]
 pub trait ReleaseFields {
-    /// Moves this struct's contents into another one,
-    /// passing each field through the function parameter.
-    /// The function requires the name of the field,
-    /// and the field's value in a `&mut Option<F>` cast to `&mut Any`.
-    fn release_fields<F>(self, give: F)
-    where F: FnMut(&'static str, &mut Any);
+    /// Swaps fields to another struct. `slot_for` is a function that returns a mutable
+    /// pointer to the field with the type of the provided string, or `null_mut` if there is no
+    /// such field. The field must be 'initialized' using `mem::zeroed()`. The second return value
+    /// is the size of the field, and is used as a sanity-check.
+    unsafe fn release_fields<F>(self, field_for: F)
+    where F: FnMut(&'static str) -> (*mut c_void, usize);
 }
 
 /// This trait indicates a type that can be locked by `context!`.
-pub trait Lockable<'u> {
-    /// A unique name for the type.
+pub unsafe trait Lockable<'u> {
+    /// It is very important that this name be unique per-type!
+    /// It is relied upon to be unique per-type.
     const TYPE_NAME: &'static str;
-
-    /// Create the lock.
     fn lock(&'u Universe) -> Self where Self: 'u;
+    // What if we got a vtable? Each type should have a unique one...
 }
 
-/// Creates a struct that holds many table locks that implement [`Lockable`].
+/// Creates a struct that holds many table locks that implement `Lockable`.
 /// This is useful for ergonomically passing multiple locks to other functions.
-/// It is possible to 'transfer' the one context's locks into another using `NewContext::from(universe, oldContext)`.
+/// It is possible to 'transfer' one context into another using `NewContext::from(universe, oldContext)`.
 /// Any unused locks will be dropped, and any new locks will be acquired.
 ///
 /// Tuples of up to three contexts can be combined. Try nesting the tuples if you need more.
@@ -54,22 +54,22 @@ pub trait Lockable<'u> {
 /// ```
 ///
 /// You might consider implementing convenience functions on the context struct.
+// This macro is Wildly Exciting.
 #[macro_export]
 macro_rules! context {
     (pub struct $name:ident {
         $(pub $i:ident: $lock:path,)*
     }) => {
-        // Shame there isn't some kind of identifier concatenation macro.
-        // I tried using `mod $name`, but forget why that doesn't work.
-        #[doc(hidden)]
-        pub mod __hi_there__please_put_each_context_in_its_own_module {
-            use std::any::Any;
+        // It's a shame there isn't some kind of identifier concatenation macro.
+        #[allow(non_snake_case)]
+        pub mod context_module {
+            use std::mem;
+            use std::ptr::null_mut;
             use $crate::context::Lockable;
-            use $crate::Universe;
 
             $(mod $i {
                 #[allow(unused)]
-                use super::super::*;
+                use super::super::*; // super::duper::*
                 // This funky business allows access to `$lock` as a type using `self::$i::Lock`,
                 // which is required due to macro restrictions.
                 pub use $lock as Lock;
@@ -81,29 +81,50 @@ macro_rules! context {
             }
             impl<'a> $name<'a> {
                 /// Create a fresh context.
-                pub fn new(universe: &'a Universe) -> Self {
+                pub fn new(universe: &'a $crate::Universe) -> Self {
                     Self {
                         $($i: $i::Lock::lock(universe),)*
                     }
                 }
             }
 
+            #[allow(unused)]
+            pub fn new(universe: &$crate::Universe) -> $name {
+                $name::new(universe)
+            }
+
             impl<'a> $crate::context::ReleaseFields for $name<'a> {
-                fn release_fields<F>(self, mut give: F)
-                where F: FnMut(&'static str, &mut Any)
+                unsafe fn release_fields<F>(self, mut field_for: F)
+                where F: FnMut(&'static str) -> (*mut ::std::os::raw::c_void, usize)
                 {
+                    // FIXME: Why c_void? Why not... T?
                     $({
-                        const NAME: &'static str = <self::$i::Lock as Lockable>::TYPE_NAME;
-                        let mut field = Some(self.$i);
-                        give(NAME, unsafe { ::std::mem::transmute(&mut field as &mut Any) });
+                        let mut field = self.$i;
+                        const TYPE_NAME: &'static str = <self::$i::Lock as Lockable>::TYPE_NAME;
+                        let (swap_to, size) = field_for(TYPE_NAME);
+                        if swap_to.is_null() {
+                            mem::drop(field);
+                        } else {
+                            let expect_size = mem::size_of::<$i::Lock>();
+                            if size != expect_size {
+                                panic!("sizes of {} did not match! {} vs {}",
+                                       <self::$i::Lock as Lockable>::TYPE_NAME, size, expect_size);
+                            }
+                            // swap_to points at invalid memory
+                            let swap_to = &mut *(swap_to as *mut $i::Lock);
+                            mem::swap(&mut field, swap_to);
+                            mem::forget(field);
+                        }
                     })*
+                    // $name'll implement Drop, which would be a problem
+                    // if we didn't move all the fields out.
                 }
             }
 
             impl<'a> $name<'a> {
                 /// Create a context from another one, recycling any locks that are in both, and
                 /// dropping any that are not.
-                pub fn from<F>(universe: &'a Universe, old: F) -> Self
+                pub fn from<F>(universe: &'a $crate::Universe, old: F) -> Self
                 where F: $crate::context::ReleaseFields
                 {
                     // We have a static list of our own fields, and we try to initialize them from
@@ -113,34 +134,46 @@ macro_rules! context {
                     $(
                         let mut $i: Option<$i::Lock<'a>> = None;
                     )*
-                    old.release_fields(|name, field| match name {
-                        $(<self::$i::Lock as Lockable>::TYPE_NAME => {
-                            if $i.is_some() {
-                                // Identical read lock from combined contexts.
-                                return;
+                    unsafe {
+                        old.release_fields(|name| {
+                            match name {
+                                $(<self::$i::Lock as Lockable>::TYPE_NAME => {
+                                    if $i.is_some() {
+                                        // This case is likely a combined table. release_fields' contract
+                                        // requires dead memory, so this test is necessary.
+                                        (null_mut(), 0)
+                                    } else {
+                                        $i = Some(mem::zeroed());
+                                        let ptr = $i.as_mut().unwrap();
+                                        (mem::transmute(ptr), mem::size_of::<$i::Lock>())
+                                    }
+                                },)*
+                                _ => (null_mut(), 0),
                             }
-                            if Some(field) = field.downcast_mut::<Option<$i::Lock<'_>>>() {
-                                $i = field.take();
-                            } else {
-                                // Odd!
-                                // 1. Two different types have the same name.
-                                // 2. Two types from different compiler versions?
-                                // We're just slightly less efficient is all.
+                        });
+                        $(
+                            if $i.is_none() {
+                                let l = <self::$i::Lock as Lockable>::lock(universe);
+                                $i = Some(l);
                             }
-                        },)*
-                        _ => (),
-                    });
+                        )*
+                    }
                     Self {
-                        $($i: if let Some(v) = $i {
-                            v
-                        } else {
-                            <self::$i::Lock as Lockable>::lock(universe)
-                        },)*
+                        $(
+                            $i: $i.unwrap(),
+                        )*
                     }
                 }
             }
+
+            #[allow(unused)]
+            pub fn from<F>(universe: &$crate::Universe, old: F) -> $name
+            where F: $crate::context::ReleaseFields
+            {
+                $name::from(universe, old)
+            }
         }
-        pub use self::__hi_there__please_put_each_context_in_its_own_module::$name;
+        pub use self::context_module::*;
     };
 }
 
@@ -148,8 +181,8 @@ mod merging_multiple_contexts {
     use super::*;
 
     impl ReleaseFields for () {
-        fn release_fields<F>(self, _give: F)
-        where F: FnMut(&'static str, &mut Any)
+        unsafe fn release_fields<F>(self, _: F)
+        where F: FnMut(&'static str) -> (*mut c_void, usize)
         {
         }
     }
@@ -158,10 +191,10 @@ mod merging_multiple_contexts {
     where
         A: ReleaseFields,
     {
-        fn release_fields<F>(self, give: F)
-        where F: FnMut(&'static str, &mut Any)
+        unsafe fn release_fields<F>(self, field_for: F)
+        where F: FnMut(&'static str) -> (*mut c_void, usize)
         {
-            self.0.release_fields(give);
+            self.0.release_fields(field_for);
         }
     }
 
@@ -170,11 +203,11 @@ mod merging_multiple_contexts {
         A: ReleaseFields,
         B: ReleaseFields,
     {
-        fn release_fields<F>(self, mut give: F)
-        where F: FnMut(&'static str, &mut Any)
+        unsafe fn release_fields<F>(self, mut field_for: F)
+        where F: FnMut(&'static str) -> (*mut c_void, usize)
         {
-            self.0.release_fields(|n, f| give(n, f));
-            self.1.release_fields(|n, f| give(n, f));
+            self.0.release_fields(|n| field_for(n));
+            self.1.release_fields(|n| field_for(n));
         }
     }
 
@@ -184,12 +217,12 @@ mod merging_multiple_contexts {
         B: ReleaseFields,
         C: ReleaseFields,
     {
-        fn release_fields<F>(self, mut give: F)
-        where F: FnMut(&'static str, &mut Any)
+        unsafe fn release_fields<F>(self, mut field_for: F)
+        where F: FnMut(&'static str) -> (*mut c_void, usize)
         {
-            self.0.release_fields(|n, f| give(n, f));
-            self.1.release_fields(|n, f| give(n, f));
-            self.2.release_fields(|n, f| give(n, f));
+            self.0.release_fields(|n| field_for(n));
+            self.1.release_fields(|n| field_for(n));
+            self.2.release_fields(|n| field_for(n));
         }
     }
 }
